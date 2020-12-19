74cb111f1d40f97749e6f7af6d5320a812d0fce8
pavel <ppa090886@icloud.com>
2020-12-19 16:27:37 +0300

    lesson1

--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 
-@section('title-block')Главная@endsection
+@section('title-block')Контакты@endsection
 
 @section('content')
-    <h1>Главная</h1>
+    <h1>Контакты</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad animi aperiam beatae corporis cum deserunt distinctio dolor doloremque eius eum ex explicabo fugiat in iste, iure iusto labore maxime minus necessitatibus officiis ratione repudiandae sapiente tempora, tenetur, unde vel voluptatum? Accusantium amet assumenda beatae consectetur corporis cumque, dolorem eaque, facilis, fugit impedit iste labore magnam molestiae optio quibusdam quisquam quos. Accusamus cum cupiditate dicta nam nemo numquam omnis placeat similique voluptatibus. Consequatur expedita in necessitatibus quas voluptate. Ad alias animi aperiam blanditiis commodi culpa doloremque dolores ducimus earum eius eos, eum eveniet harum inventore ipsa iusto labore laboriosam magnam magni maxime mollitia nulla provident quae sed sequi, suscipit veritatis vero. Animi blanditiis commodi cumque ea eaque enim et eum exercitationem expedita impedit inventore, ipsa ipsam laborum magnam maiores molestias nemo nesciunt nihil, nisi non perferendis praesentium quam quibusdam quis quos ratione repellendus repudiandae sapiente sunt tenetur. Accusantium ad, atque consequuntur corporis, culpa delectus dolorem ducimus fugiat laboriosam natus necessitatibus neque numquam officiis reiciendis repellendus sed sint. Blanditiis dolorum fugiat ipsum labore obcaecati sint tenetur. Doloribus ipsam nostrum numquam odio odit quibusdam ratione sequi vel. Ad dolorem eos exercitationem harum in ipsa molestiae nihil pariatur quidem saepe! Obcaecati, quod voluptate?</p>
 @endsection
