81fa18cdf06a6f35f87520f59d67b3f646134bf6
pavel <ppa090886@icloud.com>
2021-01-22 16:02:37 +0300

    lesson6 Вывел количество новостей в каждой категории

--- resources/views/news/categories/index.blade.php
+++ resources/views/news/categories/index.blade.php
@@ -10,7 +10,7 @@
         @foreach($categories as $category)
             <li class="list-group-item d-flex justify-content-between align-items-center">
                 <a href="{{ route('categories.show', $category->id) }}">{{ $category->title }}</a>
-                <span class="badge bg-primary rounded-pill"></span>
+                <span class="badge bg-primary rounded-pill">{{ $category->posts->count() }}</span>
             </li>
         @endforeach
     </ul>
